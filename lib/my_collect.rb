eab47ff5349fcb3f7fa9dfadded31a11cb32ceb6
Abbas Parpia <aparpia03@gmail.com>
2020-02-14 21:27:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def collect(collection)
   i = 0
   name_collection = []
   while i < collection.length
-  name_collection.push yield
+  name_collection.push yield(array[i])
+  i += 1
   
   end
 end
